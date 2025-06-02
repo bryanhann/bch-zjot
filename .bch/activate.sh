2df3ce2e361bbca31897a76d7605809bc7346f65
Bryan Hann <nobody@nowhere>
2025-06-02 13:49:18 -0230

    Use [activate.sh]

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -4,3 +4,6 @@ export BCH_ZJOT__root=$(dirname $(dirname ${BASH_SOURCE[0]}))
 export BCH_ZJOT__bin=${BCH_ZJOT__root}/.bch.bin
 export BCH_ZJOT__lib=${BCH_ZJOT__root}/.bch.lib
 export BCH_ZJOT__main=${BCH_ZJOT__bin}/bch.zjot
+
+bch:000:linkall ${BCH_ZJOT__bin}
+bch:000:link    ${BCH_ZJOT__main}
