dcc78e8f1f76d264d42d7e305be43619aa4e26f2
Bryan Hann <nobody@nowhere>
2025-06-11 16:05:54 -0230

    Insure project is [uv sync]-ed.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+r=$(dirname $(dirname $BASH_SOURCE))
+export BCH_ZJOT__root=${r}
+
 source $(dirname $BASH_SOURCE)/init/fn.sh
 ::lbin:: $(dirname $BASH_SOURCE)/lbin
 
+bch.zjot.uv-sync
+source ~/.config/bch-zjot/activate.sh
