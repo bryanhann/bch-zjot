8358b6954e3c5f093991925c7cb6703e3f275f9a
Bryan Hann <nobody@nowhere>
2025-06-11 13:09:20 -0230

    Reduce dependancy on [bch:000:*] functions.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-export BCH_ZJOT__root=$(dirname $(dirname ${BASH_SOURCE[0]}))
-export BCH_ZJOT__bin=${BCH_ZJOT__root}/.bch.bin
-export BCH_ZJOT__lib=${BCH_ZJOT__root}/.bch.lib
-export BCH_ZJOT__main=${BCH_ZJOT__bin}/bch.zjot
+source $(dirname $BASH_SOURCE)/init/fn.sh
+::lbin:: $(dirname $BASH_SOURCE)/lbin
 
-bch:000:linkall ${BCH_ZJOT__bin}
-bch:000:link    ${BCH_ZJOT__main}
